b23f5c1d222878800f511473a0500ac34c50d724
amine <amed9883@gmail.com>
2023-12-17 11:57:47 +0100

    Initial commit

---
+++ src/main/java/pl/edu/vistula/firstprojectjavascript/FirstProjectJavaScriptApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.vistula.firstprojectjavascript;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProjectJavaScriptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstProjectJavaScriptApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/edu/vistula/firstprojectjavascript/FirstProjectJavaScriptApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.vistula.firstprojectjavascript;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstProjectJavaScriptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
